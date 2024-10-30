9c8687ac69a9b1f3469d4f759262ef572a30d9c0
junggyu <jungkyu201@naver.com>
2024-10-30 11:07:31 +0900

    6666

--- thisisjava/src/ch20/oracle/sec06/BoardInsertExample.java
+++ thisisjava/src/ch20/oracle/sec06/BoardInsertExample.java
@@ -10,22 +10,36 @@ import java.io.FileInputStream;
 public class BoardInsertExample {
 
 	public static void main(String[] args) {
+		
+		// 오라클 DB board에 insert 하기
+		
+		// try 안에도 있고 finally 안에도 있어서 전역변수로
 		Connection conn = null;
+		
 		try {
 			// JDBC Driver 등록
+			// 오라클 드라이버 연결 방식 - 통신준비
 			Class.forName("oracle.jdbc.OracleDriver");
 
 			// 연결하기
-			conn = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521/XE", 
-					"c##human", 
-					"human"
+			conn = DriverManager.getConnection( // << 이 메서드를 통해 데이터베이스에 연결
+					"jdbc:oracle:thin:@localhost:1521/XE", // 오라클 연결 문자열
+					"c##human", // 아이디
+					"human" // 비번
 					);
 
 			// 매개변수화된 SQL문 작성
 			String sql = "" + "INSERT INTO boards (bno, btitle, bcontent, bwriter, bdate, bfilename, bfiledata) "
 					+ "VALUES (SEQ_BNO.NEXTVAL, ?, ?, ?, SYSDATE, ?, ?)";
+			
 			// PreparedStatement 얻기 및 값 지정
+			// JDBC ( Java Database Connectivity )에서 SQL 쿼리를 사전에 컴파일 하고 실행할 준비를
+			// 하는 객체를 생성하는 구문
+			// PreparedStatement는 SQL 쿼리에 동적으로 매개변수 값을 설정할 수 있게 하며, SQL 
+			// 인젝션 방지와 성능 향상에 유리한 방식
+			
+			// PreparedStatement를 생성할 때 사용되는 코드로, SQL 실행 후 특정 열(컬럼)의 값을
+			// 반환받기 위한 설정을 포함
 			PreparedStatement pstmt = conn.prepareStatement(sql, new String[] { "bno" });
 			pstmt.setString(1, "눈오는 날");
 			pstmt.setString(2, "함박눈이 내려요.");
--- thisisjava/src/ch20/oracle/sec08/BoardDeleteExample.java
+++ thisisjava/src/ch20/oracle/sec08/BoardDeleteExample.java
@@ -1,33 +1,29 @@
-package ch20.oracle.sec06;
+package ch20.oracle.sec08;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UserInsertExample {
+public class BoardDeleteExample {
 	public static void main(String[] args) {
-		// 데이터 삽입하기
+		// board 내용 삭제하기
 		
 		// try 안에도 있고 finally 안에도 있어서 전역변수로
 		Connection conn = null;
-		
 		try {
 			// JDBC Driver 등록
 			// 오라클 드라이버 연결 방식 - 통신준비
 			Class.forName("oracle.jdbc.OracleDriver");
 
 			// 연결하기
-			conn = DriverManager.getConnection( // << 이 메서드를 통해 데이터베이스에 연결
+			conn = DriverManager.getConnection( //<< 이 메서드를 통해 데이터베이스에 연결
 					"jdbc:oracle:thin:@localhost:1521/XE", // 오라클 연결 문자열
 					"c##human", // 아이디
 					"human" // 비번
 					);
 			
-			// 매개변수화된 SQL 문 작성
-			String sql = "" + "INSERT INTO users"
-					+ " ( userid, username, userpassword, userage, useremail ) "
-					+ "VALUES ( ?, ?, ?, ?, ?)";
+			String sql = "DELETE FROM boards WHERE bwriter=?";
 
 			// PreparedStatement 얻기 및 값 지정
 			// JDBC ( Java Database Connectivity )에서 SQL 쿼리를 사전에 컴파일 하고 실행할 준비를
@@ -36,28 +32,24 @@ public class UserInsertExample {
 			// 인젝션 방지와 성능 향상에 유리한 방식
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, "winter");
-			pstmt.setString(2, "한겨울");
-			pstmt.setString(3, "12345");
-			pstmt.setInt(4, 25);
-			pstmt.setString(5, "winter@mycompany.com");
 
-			// SQL 문 실행
 			int rows = pstmt.executeUpdate();
-			System.out.println("저장된 행 수 : " + rows);
+			System.out.println("삭제된 행 수 : " + rows);
 
 			// PreparedStatement 닫기
 			pstmt.close();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (conn != null) {
 				try {
 					// 연결 끊기
 					conn.close();
-				} catch (SQLException e) {}
+				} catch (SQLException e) {
+				}
 			}
 		}
+		
+		
 	}
 }
